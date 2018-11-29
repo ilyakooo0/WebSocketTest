b113ab03f7a08103d7c772d8739265ffde30d576
ilyakooo0 <ilyakooo0@gmail.com>
2018-11-30 02:10:44 +0300

    Added linux random numbers

--- Sources/App/Models/SafeDict.swift
+++ Sources/App/Models/SafeDict.swift
@@ -7,6 +7,14 @@
 
 import Foundation
 
+func getRandomNum(_ min: Int, _ max: Int) -> Int {
+    #if os(Linux)
+    return Int(random() % max) + min
+    #else
+    return Int(arc4random_uniform(UInt32(max)) + UInt32(min))
+    #endif
+}
+
 struct SafeDict<Key: Hashable, Value> {
     private var queue = DispatchQueue(label: "soy.iko.safeDict.queue", attributes: .concurrent)
     
@@ -33,9 +41,9 @@ struct SafeDict<Key: Hashable, Value> {
     mutating func removeRandom() -> Key? {
         var key: Key?
         queue.sync(flags: .barrier) {
-            key = dict.keys.randomElement()
-            if let key = key {
-                dict[key] = nil
+            if dict.count > 0 {
+                key = Array(dict.keys)[getRandomNum(0, dict.count)]
+                dict[key!] = nil
             }
         }
         return key
--- Sources/Run/main.swift
+++ Sources/Run/main.swift
@@ -1,3 +1,8 @@
 import App
 
+#if os(Linux)
+srandom(UInt32(time(nil)))
+#endif
+
+
 try app(.detect()).run()
